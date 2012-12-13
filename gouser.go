900a250127f009a186a3b59904e399cb55e80c0d
bophin <>
2012-12-13 21:08:20 +0100

    removed main and changed package

--- gouser.go
+++ gouser.go
@@ -1,4 +1,4 @@
-package main
+package gouser
 
 import (
 	"crypto/sha256"
@@ -18,53 +18,8 @@ type userData struct {
 var pwdfile = "pwd.txt"
 var command = "check"
 
-func main() {
-	username, password := initProgram()
-
-	switch command {
-		case "check": 
-			if CheckPwd(username, password) {
-				print("Success! logging in..\n")
-			} else {
-				print("Failure.. Invalid username or password.\n")
-			}
-		case "add": 
-			if AddUser(username, password) {
-				fmt.Printf("user added: %v\n", username)
-			} else {
-				fmt.Printf("user already exists: %v\n", username)
-			}
-		default: 
-			fmt.Printf("Error, this should not be possible\n")
-			os.Exit(1)
-	}
-}
-
 func SetPwdFile(name string) {
 	pwdfile = name
-}
-
-//Parses command line arguments and flags, and returns username and password
-//that will be used for the program.
-func initProgram() (username, password string) {
-	pwdflag := flag.String("f", "pwd.txt", "passwordfile to use")
-	cmdflag := flag.Bool("a", false, "sets add mode")
-	flag.Parse()
-	args := flag.Args()
-	
-	pwdfile = *pwdflag
-	if *cmdflag {
-		command = "add"
-	}
-
-	if len(args) != 2 {
-		fmt.Printf("Incorrect usage, write 'gouser [user] [pwd]'\n")
-		os.Exit(0)
-	}
-
-	username = args[0]
-	password = args[1]
-	return
 }
 
 //Tries to add user to the passwordfile. Returns false if the username
